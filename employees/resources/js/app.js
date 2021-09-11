78aedf12607be2c3184105941176f4e3c57d2de6
bakir1 <bakir_cutuk@hotmail.com>
2021-09-11 21:55:06 +0200

    1.4.5

--- employees/resources/js/app.js
+++ employees/resources/js/app.js
@@ -7,6 +7,9 @@
 require('./bootstrap');
 
 window.Vue = require('vue').default;
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import { routes } from "./routes";
 
 /**
  * The following block of code may be used to automatically register your
@@ -19,14 +22,16 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.use(VueRouter);
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+Vue.component('employees-index', require('./components/employees/Index.vue').default);
+
+const router = new VueRouter({
+    mode: "history",
+    routes: routes
+});
 
 const app = new Vue({
     el: '#app',
+    router: router
 });
